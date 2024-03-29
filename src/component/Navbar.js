79aa2e0b7719af297c0d385e8782d0fe3fac8983
Jinkyoung Park <qwr7652@gmail.com>
2024-03-29 19:27:40 +0900

    feat: 11~12.상품검색하기

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,7 +7,16 @@ import { useNavigate } from 'react-router-dom';
 const Navbar = () => {
   
   // 상단메뉴 array type
-  const menuList = ["여성", "Divided", "남성", "신생아/유아", "아동", "H&M Home", "Sale", "지속가능성"];
+  const menuList = [
+    "여성", 
+    "Divided", 
+    "남성", 
+    "신생아/유아", 
+    "아동", 
+    "H&M Home", 
+    "Sale", 
+    "지속가능성" 
+  ];
 
   // useNavigate가 제공해주는 함수
   const navigate = useNavigate();
@@ -15,6 +24,16 @@ const Navbar = () => {
   // 로그인페이지로 가도록
   const goToLogin = () => {
     navigate('/login');
+  };
+
+  const search = (event) => {
+    if(event.key === "Enter") {
+      // 입력한 검색어를 읽어와서 . ==> 이벤트 안에 있는, 인풋 안에있는 값을 읽어오는 방법.
+      let keyword = event.target.value;
+      
+      // url을 바꿔준다. //url에 Keyword 넣어준다.
+      navigate(`/?q=${keyword}`);
+    }
   };
 
   return (
@@ -35,14 +54,17 @@ const Navbar = () => {
       </div>
       <div className='menu-area'>
         <ul className='menu-list'>
-          {menuList.map((menu)=>(
+          {menuList.map((menu) => (
             <li>{menu}</li>
           ))}
         </ul>
 
-        <div>
+        <div className='search-box'>
             <FontAwesomeIcon icon={faSearch} />
-            <input type="text" />
+
+            {/* onKeyPress란? : 아무키나 입력하면 호출이 되는 이벤트. */}
+            {/* 모든 이벤트 리스너는 이벤트를 매개변수로 넘겨준다. */}
+            <input type="text" onKeyPress={(event)=>search(event)} /> 
         </div>
       </div>
     </div>
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import ProductCard from '../component/ProductCard';
 import { Container, Row, Col } from 'react-bootstrap';
+import ProductCard from '../component/ProductCard';
+import { useSearchParams } from 'react-router-dom';
 
 const ProductAll = () => {  
   const [productList, setProductList] = useState([]); //UI보여주려면 useState
+  const [query, setQuery] = useSearchParams();
   const getProducts = async() => {
-    let url = 'http://localhost:4000/products';
+    let searchQuery = query.get('q')||"";
+    console.log("쿼리값은?", searchQuery);
+    let url = `http://localhost:4000/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
   };
+
   useEffect(()=>{
     //함수
     getProducts();
-  },[]);
+  },[query]);
+
   return (
     <div>
       <Container>
